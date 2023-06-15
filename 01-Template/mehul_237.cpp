e25d05fdfcbd439479b88898cda67c5b51bdd704
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-06-15 20:47:22 +0530

    Create mehul_237.cpp

---
+++ 01-Template/mehul_237.cpp
@@ -0,0 +1,39 @@
+/*
+------------------------------------------------------------------------------------------------------------------------
+You are the one that is responsible for who you are!                                                                   +
+6-Months of FOCUS & HARDWORK can put you 5-YEARS ahead in life. Don't Underestimate the power of CONSISTENCY & DESIRE  +
+------------------------------------------------------------------------------------------------------------------------
+
+***********************
+ Mehul Thuletiya      *
+ CF: Mehul_Thuletiya  *
+ CC: mehul_237        *
+***********************
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define nline "\n"
+
+void solve() {
+    
+
+}
+
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t; cin>>t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+
+}
