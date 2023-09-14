89d29d08c0f873e5cbf39c089f6a9f099732d2c2
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-09-14 15:19:59 +0530

    C - Chocolate Distribution

---
+++ Codechef Contest/01-Starters/100/C.cpp
@@ -0,0 +1,68 @@
+
+// Link -> https://www.codechef.com/problems/CHOCOCHEF
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	
+	int n;
+	cin >> n;
+	
+	cout << 1 << " " << 1 << " " << n - 2 << endl;
+	
+}
+
+int main() {
+    
+	int t = 1;
+	cin >> t;
+	
+	for(int _t = 1; _t <= t; _t++) {
+		solve();
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+// -------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define nline "\n"
+const int MOD = 1e9 + 7;
+
+
+void solve() {
+    
+    int jm;
+    cin >> jm;
+    
+    cout << 1 << ' ' << 1 << ' ' << jm - 2 << "\n"; 
+
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  
+  int t1; 
+  cin>>t1;
+
+  while(t1--) {
+    solve();
+  } 
+
+  return 0;
+}
