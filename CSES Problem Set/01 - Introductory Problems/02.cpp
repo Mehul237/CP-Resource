d55832d96b73ee609877b9b0258da9dada2863f4
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-09-16 22:51:16 +0530

    02 - Missing Number

--- CSES Problem Set/01 - Introductory Problems/02.cpp
+++ CSES Problem Set/01 - Introductory Problems/02.cpp
@@ -8,25 +8,34 @@ typedef long long ll;
 #define nline "\n"
 const int MOD = 1e9 + 7;
 
-
 int main() {
-
+  
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
    
+  // Declare variables N and sum
   ll N, sum = 0;
+
+  // Input the value of N
   cin >> N;
   
-  for(int i=1; i<N; ++i) {
+  // Loop to read N-1 numbers
+  for(int i = 1; i < N; ++i) {
+    
     int ans;
+    
+    // Input the next number in the sequence
     cin >> ans;
 
+    // Add the number to the running sum
     sum += ans;
   }
 
-  cout << N*(N+1)/2 - sum;
-
+  // Calculate the missing number using the formula for the sum of the first N natural numbers
+  // and subtract the sum of the entered N-1 numbers
+  cout << N * (N + 1) / 2 - sum;
 
   return 0;
 }
+
