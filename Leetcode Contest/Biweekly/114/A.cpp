1a5a2e655cb0405a398a4b4f840d6d6003b03487
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-10-01 08:51:58 +0530

    2869. Minimum Operations to Collect Elements

--- Leetcode Contest/Biweekly/114/A.cpp
+++ Leetcode Contest/Biweekly/114/A.cpp
@@ -1,18 +1,30 @@
-
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-      
-        set <int> values;
-        int moves = 0;
-      
-        for(int i = nums.size() - 1;i>=0;--i) {
-            moves++;
-            if(nums[i] <= k) {         
-                values.insert(nums[i]);
+
+        // A set to keep track of the unique values collected.
+        set<int> values;  
+        // Variable to count the number of moves (operations).
+        int moves = 0;    
+
+        for (int i = nums.size() - 1; i >= 0; --i) {
+
+             // Increment the number of moves for each operation.
+            moves++; 
+
+            // If the current element is less than or equal to k.
+            if (nums[i] <= k) {  
+
+                // Add the element to the collection (set).
+                values.insert(nums[i]);  
+
+                // If the collection contains all elements from 1 to k, return the number of moves.
+                if (values.size() == k) 
+                    return moves;
             }
-            if(values.size() == k) return moves;
         }
-        return 0;
+        
+         // If it's not possible to collect elements 1 to k, return 0.
+        return 0; 
     }
 };
